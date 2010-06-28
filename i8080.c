fb60da59460f71c194cfd924751766eca1c6ec25
LittleCodeShop <coders@littlecodeshop.com>
2010-06-28 08:57:41 +0200

    Added a main so now all is ok to integrate

--- i8080.c
+++ i8080.c
@@ -10,58 +10,12 @@
 
 #include "i8080.h"
 
-I8080_CPU *cpu;
 
 int starttrace=0;
 
 
-#define LCS_DEBUG
-/* Macro for printing debug info */
-#ifdef LCS_DEBUG
-#define LCS_DPRINT(fmt, args...)    fprintf(stderr,fmt, ## args)
-#else
-#define LCS_DPRINT(fmt, args...)    /* Don't do anything in release builds */
-#endif
+//I8080_CPU *cpu;
 
-
-/* Bit manipulation and parity tables */
-#define BIT(n)                  ( 1<<(n) )
-
-#define BIT_SET(y, mask)        ( y |=  (mask) )
-#define BIT_CLEAR(y, mask)      ( y &= ~(mask) )
-#define BIT_FLIP(y, mask)       ( y ^=  (mask) )
-
-
-
-
-/*
-   Set bits        Clear bits      Flip bits
-   y        0x0011          0x0011          0x0011
-   mask     0x0101 |        0x0101 &~       0x0101 ^
-   ---------       ----------      ---------
-   result   0x0111          0x0010          0x0110
-   */
-
-////////////////////////////////////////////////////////////////////////////////
-//    Examples:
-//    mask= BIT(0) | BIT(8);      // Create mask with bit 0 and 8 set (0x0101)
-
-//    BIT_SET(y, mask);           // Bits 0 and 8 of y have been set.
-//    BIT_CLEAR(y, mask);         // Bits 0 and 8 of y have been cleared.
-//    BIT_FLIP(y, mask);          // Bits 0 and 8 of y have been flipped.
-
-
-unsigned char ParityTable256[256] = 
-{
-#   define P2(n) n, n^1, n^1, n
-#   define P4(n) P2(n), P2(n^1), P2(n^1), P2(n)
-#   define P6(n) P4(n), P4(n^1), P4(n^1), P4(n)
-    P6(0), P6(1), P6(1), P6(0)
-};
-
-/* Tx Rx values */
-#define RxStat_BIT	0x01	// Ready to receive
-#define TxStat_BIT	0x02	// Ready to transmit
 
 /* i8080 CPU */
 /* opcode cycles */
@@ -109,6 +63,12 @@ static const char* lut_mnemonic[0x100]={
 };
 #endif
 
+
+
+
+/* RUN CPU OPCODES */
+void cpu_run( I8080_CPU * cpu, int cycles)
+{
 
 #define A               cpu->reg.a
 #define F               cpu->reg.flags
@@ -164,87 +124,7 @@ static const char* lut_mnemonic[0x100]={
 #define XRA(r)          RES=A=A^r
 #define ORA(r)          RES=A=A|r
 
-static unsigned char key=0;
-unsigned char buffer[3];
-int bcount = 0;
 
-static unsigned char in_port(unsigned char port)
-{
-    unsigned char ret=port;
-
-    //LCS_DPRINT("--> IN port %X\n",port);
-    switch (port) {
-	case 0x00:
-	    break;
-	case 0x10:    
-	    ret = TxStat_BIT;
-	    /*if(_kbhit()){
-	    //check if it is a control caracter for DSK
-	    char c = _getch();
-	    buffer[0] = c;
-	    bcount = 1;
-	    }*/
-	    if(bcount)
-		ret = ret|RxStat_BIT;
-	    break;
-	case 0x01:
-	    if(bcount>0){
-		ret = buffer[0];
-		bcount = 0;
-	    }
-	    break;
-	case 0x11:    
-	    {
-		unsigned char b = buffer[0];
-		unsigned char parity = ParityTable256[b];
-		if(parity){
-		    unsigned char mask=0x80;
-		    BIT_SET(b, mask); 
-		}
-		ret = b;
-		bcount = 0;
-	    }
-	    break;
-	default: 
-	    ret=0x0;
-	    break;
-    }
-
-    return ret;
-}
-
-static void out_port(unsigned char port,unsigned char v)
-{
-    switch (port) {
-
-	case 0x1:
-	case 0x11:
-	    //LCS_DPRINT("%c<-- OUT port %X\n",v&0x7F,port);
-	    printf("%c",v&0x7F);
-	    fflush(stdout);
-	    break;
-	default: 
-	    //   LCS_DPRINT("%c<-- OUT port %X\n",v,port);
-	    break;
-
-    }
-}
-
-/* INTERRUPTS */
-static void interrupt(int i)
-{
-    LCS_DPRINT("INTERUPTION\n");
-    if (cpu->i) {
-	cpu->i=cpu->ipend=0;
-	cpu->cycles-=11;
-	RST(i);
-    }
-    else cpu->ipend=0x80|i;
-}
-
-/* RUN CPU OPCODES */
-static void cpu_run(int cycles)
-{
     int opcode;
 
     cpu->cycles+=cycles;
@@ -569,13 +449,13 @@ static void cpu_run(int cycles)
 
 
 		       /* INPUT/OUTPUT */
-	    case 0xd3: out_port(R8(PC),A); PC++; break;     /* out p */
-	    case 0xdb: A=in_port(R8(PC)); PC++; break;      /* in p */
+	    case 0xd3: cpu->out_port_ptr(R8(PC),A); PC++; break;     /* out p */
+	    case 0xdb: A=cpu->in_port_ptr(R8(PC)); PC++; break;      /* in p */
 
 
 		       /* CONTROL */
 	    case 0xf3: cpu->i=0; break;                      /* di */
-	    case 0xfb: cpu->i=1; if (cpu->ipend&0x80) interrupt(cpu->ipend&0x7f); break;       /* ei */
+	    case 0xfb: cpu->i=1; if (cpu->ipend&0x80) cpu->interrupt_ptr(cpu->ipend&0x7f); break;       /* ei */
 	    case 0x00: break;                               /* nop */
 	    case 0x76: cpu->cycles=0; break;                 /* hlt (mov M,M) */
 
@@ -588,111 +468,9 @@ static void cpu_run(int cycles)
 }
 
 
-void loadCoreMem(char *file)
-{
-    FILE * fp;
-    fp = fopen(file,"rb");
-    if(fp){
-	//get size of file
-	fseek(fp, 0, SEEK_END);
-	long fileSize = ftell(fp);
-	rewind(fp);
-	fread(cpu->mem,1,fileSize,fp);	
 
-    }
-    else { //ERROR
-	LCS_DPRINT("Cannot read bin file \n");
-    }
+void initCPU(I8080_CPU * cpu){
 
-}
-
-void emuRun(void){
-    cpu_run(1000);
-}
-
-void init(void) 
-{
-    glClearColor (0.0, 0.0, 0.0, 0.0);
-    glClearDepth(1.0);
-    glShadeModel (GL_SMOOTH);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_LIGHT0);
-}
-void drawAxis()
-{
-    glPushMatrix();
-    // Je veux pas de lightning ici
-    glDisable(GL_LIGHTING);
-    //I draw 2 axis
-    glColor3f(1.0,0.0,0.0);
-    glBegin(GL_LINES);
-
-    glColor3f(1.0, 0.0, 0.0);                  /* red */ 
-    glVertex3f(-5.0,0.0,0.0);
-    glVertex3f(5.0,0.0,0.0);
-
-    glColor3f(0.0, 1.0, 0.0);                  /* Green */ 
-    glVertex3f(0.0,-5.0,0.0);
-    glVertex3f(0.0,5.0,0.0);
-
-    glColor3f(0.0, 0.0, 1.0);                  /* Blue */ 
-    glVertex3f(0.0,0.0,-5.0);
-    glVertex3f(0.0,0.0,5.0);
-
-    glEnd();
-    glEnable(GL_LIGHTING);
-    glPopMatrix();     
-
-}
-void display(void)
-{
-
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-
-    drawAxis();
-
-
-    glutSwapBuffers();
-
-
-}
-void reshape(int w, int h)
-{
-    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-5.0, 5.0, -5.0, 5.0, -5.0, 5.0);
-    //glFrustum (-5.0, 5.0, -5.0, 5.0, 2.0, 10.0);
-
-    glMatrixMode(GL_MODELVIEW);
-    //glLoadIdentity();
-}
-void mouse(int button, int state, int x, int y) 
-{
-    switch (button) {
-	case GLUT_LEFT_BUTTON:
-	    break;
-	case GLUT_RIGHT_BUTTON:
-	    break;
-	default:
-	    break;
-    }
-}
-
-    void keyboard(unsigned char key, int x, int y) {
-	if (key == 27) 
-	    exit(0);
-	else
-	{
-	    buffer[0] = key;
-	    bcount=1;
-	}
-    }
-
-void initCPU(I8080_CPU * acpu){
-
-    cpu = acpu;
 
     //Create a lookup table for the parity
     int i;
@@ -701,28 +479,4 @@ void initCPU(I8080_CPU * acpu){
     PC = 0x0;
 }
 
-int main(int argc, char** argv){
 
-    I8080_CPU *icpu = malloc(sizeof(I8080_CPU));
-    unsigned char * amem = malloc(0XFFFF);
-    initCPU(icpu);
-    icpu->out_port_ptr = out_port;
-    icpu->in_port_ptr = in_port;
-    icpu->mem = amem;
-
-    loadCoreMem("4kbas.bin");
-    glutInit(&argc, argv);
-    //NOTE: pour avoir du zbuffer il suffit de mettre GLUT_DEPTH ici, dans les trucs iphone il faut mettre le define USE_DEPTH_BUFFER
-    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGBA|GLUT_DEPTH);
-    glutInitWindowSize (250, 250); 
-    glutInitWindowPosition (100, 100);
-    glutCreateWindow (argv[0]);
-    init ();
-    glutDisplayFunc(display); 
-    glutReshapeFunc(reshape); 
-    glutMouseFunc(mouse);
-    glutKeyboardFunc(keyboard);
-    glutIdleFunc(emuRun);
-    glutMainLoop();
-    return 0;
-}
--- i8080.h
+++ i8080.h
@@ -1,5 +1,14 @@
 #ifndef LCS_I8080_H
 #define LCS_I8080_H
+
+#define LCS_DEBUG
+/* Macro for printing debug info */
+#ifdef LCS_DEBUG
+#define LCS_DPRINT(fmt, args...)    fprintf(stderr,fmt, ## args)
+#else
+#define LCS_DPRINT(fmt, args...)    /* Don't do anything in release builds */
+#endif
+
 typedef struct {
 
     union {
@@ -25,10 +34,11 @@ typedef struct {
     unsigned char * mem;                             /* Core */
 
     void (* out_port_ptr)(unsigned char port,unsigned char v); /* port output */
-    unsigned char (* in_port_ptr)(unsigned char port); 		/*port input */
+    unsigned char (* in_port_ptr)(unsigned char port); 		/* port input */
+    void (* interrupt_ptr)(int i); 		/* interruption */
 
 } I8080_CPU;
 
 void initCPU( I8080_CPU * acpu);
-static void cpu_run(int cycles);
+void cpu_run(I8080_CPU * cpu, int cycles);
 #endif 
